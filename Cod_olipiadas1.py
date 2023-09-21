318604ecd0529d663bda6ba9d15ac40be00577c3
ValSerrano3 <145025949+ValSerrano3@users.noreply.github.com>
2023-09-21 08:04:30 -0600

    Create Cod_olipiadas1.py

---
+++ Cod_olipiadas1.py
@@ -0,0 +1,13 @@
+def suma(a,b):
+  x = a + b
+  return x
+def resta(a,b):
+  x = a - b
+  return x
+
+
+print("Dame el primer nuemro:")
+a= int(input())
+print("Dame el segundo nuemro:")
+b= int(input())
+print("La suma da", suma(a,b),"y la resta da",resta(a,b))
